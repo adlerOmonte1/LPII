6e4f494f6925ada736d090ad8d7fe694fbaea187
adlerOmonte1 <klin.forever24@gmail.com>
2025-12-06 08:31:34 -0500

    integracion completa

--- config/conexion.php
+++ config/conexion.php
@@ -11,7 +11,7 @@ class Conexion {
         string $host = "localhost",
         string $dbname = "InstitutoIdiomas",
         string $username = "root",
-        string $password = "2022210277"
+        string $password = "root"
     ) {
         $this->dsn = "mysql:host={$host};dbname={$dbname};charset=utf8mb4";
         $this->username = $username;
